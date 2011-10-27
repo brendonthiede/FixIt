a9ed12927c45c996070028236293a4acd3b9e5c6
Brendon Thiede <brendon.thiede@gmail.com>
2011-10-27 16:09:36 -0400

    initial Maven project structure commit

---
+++ src/main/java/com/vertafore/FixIt/App.java
@@ -0,0 +1,13 @@
+package com.vertafore.FixIt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
